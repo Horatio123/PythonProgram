99ed5e0cd89eac189590860cd6ecd482fbb3853d
Horatio <alexanderchen1992@gmail.com>
2019-09-03 23:39:00 +0800

    slice and if

---
+++ firstTest/ifTest.py
@@ -0,0 +1,20 @@
+value = 0
+if value > 0:
+    print "input value is: ", value, "larger than 0"
+elif value == 0:
+    print "input value is 0"
+else:
+    print ("input value is smaller than 0")
+    print ("666")
+
+print ("not related to if statement")
+
+print "\n"
+
+if value >= 0:
+    print "input value is: ", value, "larger than or equal to 0"
+    if value == 0:
+        print "input value is 0"
+else:
+    print ("input value is smaller than 0")
+    print ("666")
---
+++ firstTest/sliceTest.py
@@ -0,0 +1,12 @@
+numList = [0, 1, 2, 3, 4]
+
+print numList[2:4]
+print numList[2:]
+print numList[:5]
+print numList[-2:]
+print numList[:-2]
+print numList[:]
+print numList[::2]
+print numList[1:5:2]
+print numList[::-2]
+print numList[-1:-5:-2]
